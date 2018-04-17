931e0ecefa54286ffcdc179c8b61a2fb4b91dac9
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-17 12:42:40 +0100

    Create wrapper component

---
+++ src/atoms/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.View`
+    flex: 1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+`;
+
+export default Wrapper;
