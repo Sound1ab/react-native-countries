a48279b632d091923b5be90c703ebf87f3b8bb77
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-17 12:42:15 +0100

    Create button

---
+++ src/atoms/Button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+const Button = styled.TouchableOpacity`
+    padding: 16px 40px;
+    background-color: tomato;
+    border-radius: 5px;
+`;
+
+export default Button;
