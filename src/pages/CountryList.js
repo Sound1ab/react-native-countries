a63a4194197214880d519f2e981837b6aee53e6f
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-17 12:43:01 +0100

    Create country list component

--- src/pages/CountryList.js
+++ src/pages/CountryList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import {Text, View} from 'react-native';
+import {Text, SectionList, FlatList} from 'react-native';
 import PropTypes from 'prop-types';
 
 const StyledText = styled.Text`
@@ -10,35 +10,55 @@ const StyledText = styled.Text`
 class CountryList extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            loading: true,
+            error: false,
+            countries: []
+        };
     }
 
-    componentDidMount () {
-        console.log(this.props);
-    }
+    static navigationOptions = {
+        title: 'Countries'
+    };
 
-    handlePress = () => {
-       const {navigation} = this.props;
-       navigation.navigate('Home');
+    componentWillMount = async () => {
+        try {
+            const response = await fetch('https://restcountries.eu/rest/v2/all');
+            const countries = await response.json();
+            const formattedData = this.createKeys(countries);
+            this.setState({
+                loading: false,
+                countries: formattedData
+            });
+        } catch (e) {
+            console.log(e);
+            this.setState({loading: false, error: true});
+        }
+    };
+
+    createKeys = (data) => {
+        return data.map(el => {
+            return {
+                ...el,
+                key: el.name
+            }
+        })
+    };
+
+    renderItem = ({item, index}) => {
+        console.log(item, index);
+        return <Text key={index}>{item.key}</Text>;
     };
 
     render() {
+        const {countries} = this.state;
         return (
-            <StyledText
-                onPress={this.handlePress}
-            >
-                hey
-            </StyledText>
+            <FlatList
+                data={countries}
+                renderItem={this.renderItem}
+            />
         );
     }
 }
-
-CountryList.propTypes = {
-
-};
-
-CountryList.defaultProps = {
-
-};
 
 export default CountryList;
