09e376a25e41dd98cd0f6725567180122158c558
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-17 15:49:33 +0100

    Update text so that it accepts color prop

--- src/atoms/Text.js
+++ src/atoms/Text.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Text = styled.Text`
-    color: white;
+    color: ${({color}) => color};
     font-family: 'System';
     font-weight: 200;
 `;
+
+Text.propTypes = {
+    color: PropTypes.string
+};
+
+Text.defaultProps = {
+    color: 'white'
+};
 
 export default Text;
